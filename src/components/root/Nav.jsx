eb10fd2c8f8547da1edd515f3011462f7d8e9c20
Jose Barrera <jfbarrera2002@gmail.com>
2025-05-15 09:16:47 -0500

    Implemented navigation bar

--- src/components/root/Nav.jsx
+++ src/components/root/Nav.jsx
@@ -1,25 +1,31 @@
 import { NavLink, Link } from "react-router";
+import { useState } from 'react';
 import '@styles/Nav.css';
 import Logo from '@assets/Logo.svg';
+import HamburgerIcon from '@assets/🦆 icon _hamburger menu.svg';
 
 // the top-level navigation of the website, sometimes referred to as the navbar
 const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return(
         <nav className="main-nav">
             <div className="nav-logo">
                 <Link to="/">
-                    <img src={Logo} alt="Logo" className="logo" />
+                    <img src={Logo} alt="Little Lemon" className="logo" />
                 </Link>
             </div>
-            <ul className="nav-links">
+            
+            <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
                 <li>
                     <NavLink to="/" end>Home</NavLink>
                 </li>
                 <li>
                     <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contact">Contact</NavLink>
                 </li>
                 <li>
                     <NavLink to="/menu">Menu</NavLink>
@@ -34,6 +40,10 @@ const Nav = () => {
                     <NavLink to="/login">Login</NavLink>
                 </li>
             </ul>
+            
+            <button className="mobile-menu-button" onClick={toggleMenu}>
+                <img src={HamburgerIcon} alt="Menu" className="hamburger-icon" />
+            </button>
         </nav>
     );
 };
